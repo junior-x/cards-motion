6fcaaacdfaf2ac3e0d9428b88afe543593ed669a
junior-x <marcosmansur@outlook.com>
2022-02-09 19:06:26 -0800

    cards

---
+++ src/card.js
@@ -0,0 +1,12 @@
+import './card.css'
+
+export default function Card() {
+	
+	return(
+		<div className='cardBG'>
+			<div className='rowShort'></div>
+			<div className='row'></div>
+			<div className='rowMed'></div>
+		</div>
+	)
+}
---
+++ src/cardSmall.js
@@ -0,0 +1,12 @@
+import './cardSmall.css'
+
+export default function CardSmall() {
+	
+	return(
+		<div className='cardBGSmall'>
+			<div className='rowShortSmall'></div>
+			<div className='rowSmall'></div>
+			<div className='rowMedSmall'></div>
+		</div>
+	)
+}
